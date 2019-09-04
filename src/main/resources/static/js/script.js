adc1d662bd1d6b20f5adae2ef1432eeab7b355ee
axunga <axunga.toga94@gmail.com>
2019-09-04 12:18:08 -0500

    axel todas las bebidas funcionando

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,18 +1,55 @@
 let xhr = new XMLHttpRequest();
+let total = 500.50;
 
 window.onload = function()
 {
-	//alert("funcionando");
-	var btnAgregar = document.querySelector("#btnAgregar");
-	btnAgregar.onclick = enviarPet;
+	var btnAgregarArizona = document.querySelector("#btnAgregarArizona");
+	btnAgregarArizona.onclick = enviarPetArizona;
+	
+	var btnAgregarRefresco = document.querySelector("#btnAgregarRefresco");
+	btnAgregarRefresco.onclick = enviarPetRefresco;
+	
+	var btnAgregarCafe = document.querySelector("#btnAgregarCafe");
+	btnAgregarCafe.onclick = enviarPetCafe;
+	
+	console.log()
 }
-
-function enviarPet()
+//call get Arizona
+function enviarPetArizona()
 {
-	//alert("tipo bebida request");
     console.log("boton picado" + btnAgregar.value);
-    var opcion = document.querySelector("#opcion").value;
-    xhr.open("GET", "producto/nombre/"+opcion);
+    var opcion = document.querySelector("#opcionArizona").value;
+    var comentarios = document.querySelector("#comentariosArizona").value;
+    
+    console.log(opcion, comentarios);
+    
+    xhr.open("GET", "producto/nombre/" + opcion);
+	xhr.onload = functionCallBack;
+	xhr.send(null);
+}
+//call get refresco
+function enviarPetRefresco()
+{
+    console.log("boton picado" + btnAgregar.value);
+    var opcion = document.querySelector("#opcionRefresco").value;
+    var comentarios = document.querySelector("#comentariosRefresco").value;
+    
+    console.log(opcion, comentarios);
+    
+    xhr.open("GET", "producto/nombre/" + opcion);
+	xhr.onload = functionCallBack;
+	xhr.send(null);
+}
+//call get cafe
+function enviarPetCafe()
+{
+    console.log("boton picado" + btnAgregar.value);
+    var opcion = document.querySelector("#opcionCafe").value;
+    var comentarios = document.querySelector("#comentariosCafe").value;
+    
+    console.log(opcion, comentarios);
+    
+    xhr.open("GET", "producto/nombre/" + opcion);
 	xhr.onload = functionCallBack;
 	xhr.send(null);
 }
@@ -20,9 +57,17 @@ function enviarPet()
 function functionCallBack()
 {
     console.log(xhr.status);
-    console.log(opcion, comentarios);
+    console.log(xhr.responseText);
+    
 	if(xhr.status == 200)
-	{
-		document.querySelector("#miOrdenResultado").innerHTML = xhr.responseText;
+	{	
+		var jsonReturned = JSON.parse(xhr.responseText);
+		var displayNombre = "<p><h5>" + jsonReturned["nombre"] + ", " + jsonReturned["tipoBebida"] +"</h5></p>";
+		var displayPrecio = "<p><h5>" + jsonReturned["precio"] + "</h5></p>";
+		total += jsonReturned["precio"];
+		
+		document.querySelector("#miOrdenNombre").innerHTML += displayNombre;
+		document.querySelector("#miOrdenPrecio").innerHTML += displayPrecio;
+		document.querySelector("#miOrdenTotal").innerHTML = total;
 	}
 }
